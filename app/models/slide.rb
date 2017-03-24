67dd6cab048f885466051386920f2e11ff1d3851
Brad Thompson <bkt@brad-thompson.com>
2017-03-23 20:52:18 -0400

    change

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -4,7 +4,8 @@ class Slide < ApplicationRecord
   has_attached_file :attachment, styles: {preview: '300x300>', full: '1280x720>'}#, processors: [:artist_watermark]
   validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\z/     
 
-  has_many :decks_slides
+  has_many :decks_slides, dependent: :destroy
   has_many :decks, through: :decks_slides
 
+  validates :name, uniqueness: true
 end
